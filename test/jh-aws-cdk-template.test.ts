b78278febde147f109178d0631baa6c7b3e46364
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-12-17 21:10:31 +0900

    📦(dep): Add package-lock.json to initialize project dependencies
    Introduces the `package-lock.json` file to manage and lock down dependency versions in the project for consistency. This includes all required dependencies and development tools for the project setup.

---
+++ test/jh-aws-cdk-template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JhAwsCdkTemplate from '../lib/jh-aws-cdk-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/jh-aws-cdk-template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new JhAwsCdkTemplate.JhAwsCdkTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
